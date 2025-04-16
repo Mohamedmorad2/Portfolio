ea25adbc3653dfd59b14c00de18a41031868c708
Mohamedmorad2 <m7medmorad12@gmail.com>
2025-04-16 19:38:45 +0200

    Updata File CV

--- Header/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const header = document.getElementById('main-header');
-    const toggleBtn = document.getElementById('toggle-btn');
-
-    toggleBtn.addEventListener('click', function () {
-        // Toggle background color on button click
-        header.style.backgroundColor = header.style.backgroundColor === 'rgb(51, 51, 51)' ? '#4CAF50' : '#333';
-    });
-});
